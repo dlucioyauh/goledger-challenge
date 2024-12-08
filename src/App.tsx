dd3ba6ad4e02560356dddfcc9b6fe47995028970
dlucioyauh <dlucio.douglas@gmail.com>
2024-12-08 19:55:48 -0300

    Criando estrutura inicial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+      <div>
+          <h1>GoLedger Challenge</h1>
+          <p>Welcome to the blockchain streaming app!</p>
+      </div>
+  );
+};
+
+export default App;
